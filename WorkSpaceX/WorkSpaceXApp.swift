8ee5c42a4a8e0a8a900c333c917c6de50f21c787
Kim JaeHyung <you3192001@icloud.com>
2024-06-04 01:21:53 +0900

    Initial Commit

---
+++ WorkSpaceX/WorkSpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkSpaceXApp.swift
+//  WorkSpaceX
+//
+//  Created by Jae hyung Kim on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkSpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
