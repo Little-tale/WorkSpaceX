53fd2116f16213a9fff01f0042889b0f8f8bf3a3
Kim JaeHyung <you3192001@icloud.com>
2024-06-06 00:24:58 +0900

    Fix: 중복확인을 여러번 누를 경우 토스트가 사라지지 않던 버그를 수정합니다.

--- WorkSpaceX/Sources/Views/Toast/StaticToastColorView.swift
+++ WorkSpaceX/Sources/Views/Toast/StaticToastColorView.swift
@@ -22,7 +22,7 @@ struct StaticToastColorView: View {
     
     var body: some View {
         Group {
-            if let text {
+            if let text, isVisible {
                 Text(text)
                     .padding(.horizontal, 10)
                     .frame(height: 40)
@@ -31,47 +31,31 @@ struct StaticToastColorView: View {
                     .background(color)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                     .onAppear {
+                        print("....isVisible \(isVisible)")
                         startTimer()
                     }
             } else {
                 EmptyView()
-                    .onAppear {
-                        timer?.invalidate()
-                    }
+            }
+        }
+        .onChange(of: text) { newValue in
+            if newValue != nil {
+                isVisible = true
+                startTimer()
             }
         }
     }
     
     private func startTimer() {
+        // isVisible = false
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: duration, repeats: false) { _ in
             withAnimation {
-                isVisible = false
+                isVisible = true
                 text = nil
             }
         }
     }
 }
 
-/*
- // 작동 하지만 라이브러리로도 구현 해보기.
- private func ifCurrectView(text: String?) -> some View {
-     // 각 버튼별로 로직이 다름을 생각해야함.
-     // 이친구는 Text를 받아야 할것 같음
-     withAnimation {
-         Group {
-             if let text {
-                 Text(text)
-                     .padding(.horizontal, 10)
-                     .frame(height: 40)
-                     .foregroundStyle(WSXColor.white)
-                     .font(WSXFont.bodyBold)
-                     .background(WSXColor.green)
-                     .clipShape(RoundedRectangle(cornerRadius: 12))
-             } else {
-                 EmptyView()
-             }
-         }
-     }
- }
- */
+
