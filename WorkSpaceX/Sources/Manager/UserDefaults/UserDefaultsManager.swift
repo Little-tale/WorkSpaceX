d27bacce44ccae10d7d7808deb92330f18fb74c3
Kim JaeHyung <you3192001@icloud.com>
2024-06-07 14:47:04 +0900

    Fetch: UserDefaultsManager

---
+++ WorkSpaceX/Sources/Manager/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,36 @@
+//
+//  UserDefaultsManager.swift
+//  WorkSpaceX
+//
+//  Created by Jae hyung Kim on 6/7/24.
+//
+
+import Foundation
+
+
+enum UserDefaultsManager {
+    
+    enum Key: String {
+        case deviceToken
+        case userID
+        case accessToken
+        case refreshToken
+        
+        var value: String {
+            return self.rawValue
+        }
+    }
+    
+    @UserDefaultsWrapper(key: Key.userID.value, placeValue: "")
+    static var userID: String
+    
+    @UserDefaultsWrapper(key: Key.deviceToken.value, placeValue: "")
+    static var deviceToken: String
+    
+    @UserDefaultsWrapper(key: Key.accessToken.value, placeValue: "")
+    static var accessToken: String
+    
+    @UserDefaultsWrapper(key: Key.refreshToken.value, placeValue: "")
+    static var refreshToken: String
+    
+}
