0ac1b4ab6f4c6b3c066e01353277d35bbe901cec
Kim JaeHyung <you3192001@icloud.com>
2024-06-30 18:07:29 +0900

    Fix: 스크롤 뷰 리더 이슈 + 뷰 추적 안됨 해결

--- WorkSpaceX/Sources/Views/Chatting/WorkSpaceChannelChattingView.swift
+++ WorkSpaceX/Sources/Views/Chatting/WorkSpaceChannelChattingView.swift
@@ -24,25 +24,25 @@ struct WorkSpaceChannelChattingView: View {
             
             VStack {
                 ScrollViewReader { proxy in
-                    ScrollView {
-                        LazyVStack {
-
-                            ForEach(store.currentModels, id: \.testID) { send in
-                                ChatModeView(
-                                    setModel: send,
-                                    profileClicked: { reModel in
-                                        
-                                    },
-                                    fileClicked: { urlString in
-                                        
-                                    }
-                                )
+                    WithPerceptionTracking {
+                        ScrollView {
+                            LazyVStack {
+                                
+                                ForEach(store.currentModels, id: \.testID) { send in
+                                    ChatModeView(
+                                        setModel: send,
+                                        profileClicked: { reModel in
+                                            
+                                        },
+                                        fileClicked: { urlString in
+                                            
+                                        }
+                                    )
+                                }
                             }
+                            .rotationEffect(.radians(.pi))
+                            .scaleEffect(x: -1, y: 1, anchor: .center)
                         }
-                        .rotationEffect(.radians(.pi))
-                        .scaleEffect(x: -1, y: 1, anchor: .center)
-                        
-        
                     }
                     .rotationEffect(.radians(.pi))
                     .scaleEffect(x: -1, y: 1, anchor: .center)
@@ -133,11 +133,9 @@ struct WorkSpaceChannelChattingView: View {
 extension WorkSpaceChannelChattingView {
     
     func chatTextField() -> some View {
-        WithPerceptionTracking {
-            Group {
-                workSpaceToolView()
-                workSpaceTextField()
-            }
+        Group {
+            workSpaceToolView()
+            workSpaceTextField()
         }
     }
     
@@ -145,49 +143,47 @@ extension WorkSpaceChannelChattingView {
 extension WorkSpaceChannelChattingView {
     
     func workSpaceTextField() -> some View {
-        WithPerceptionTracking {
+        VStack {
             VStack {
-                VStack {
-                    HStack {
-                        WSXImage.plus
-                            .resizable()
-                            .renderingMode(.template)
-                            .frame(width: 24, height: 24)
-                            .foregroundStyle(WSXColor.black)
-                            .padding(.leading, 8)
-                            .asButton {
-                                keyboardTool.toggle()
-                            }
-                        VStack {
-                            TextField("메시지를 입력하세요", text: $store.userFeildText.sending(\.userFeildText), axis: .vertical)
-                                .lineLimit(4)
-                                .padding(.vertical, 2)
+                HStack {
+                    WSXImage.plus
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(WSXColor.black)
+                        .padding(.leading, 8)
+                        .asButton {
+                            keyboardTool.toggle()
                         }
-                        .frame(minHeight: 50)
-                        
-                        WSXImage.send
-                            .resizable()
-                            .renderingMode(.template)
-                            .aspectRatio(1, contentMode: .fit)
-                            .frame(width: 24, height: 24)
-                            .asButton {
-                                store.send(.sendTapped)
-                            }
-                            .padding(.trailing, 5)
+                    VStack {
+                        TextField("메시지를 입력하세요", text: $store.userFeildText.sending(\.userFeildText), axis: .vertical)
+                            .lineLimit(4)
+                            .padding(.vertical, 2)
                     }
-                   
-                    if store.showChatBottom {
-                        workSpaceChatBottomItemView()
-                            .padding(.bottom, 4)
-                    }
+                    .frame(minHeight: 50)
+                    
+                    WSXImage.send
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .asButton {
+                            store.send(.sendTapped)
+                        }
+                        .padding(.trailing, 5)
                 }
-                .background {
-                    RoundedRectangle(cornerRadius: 18)
-                        .fill(WSXColor.black.opacity(0.1))
+               
+                if store.showChatBottom {
+                    workSpaceChatBottomItemView()
+                        .padding(.bottom, 4)
                 }
-                .padding(.horizontal, 8)
-                .padding(.bottom, 4)
             }
+            .background {
+                RoundedRectangle(cornerRadius: 18)
+                    .fill(WSXColor.black.opacity(0.1))
+            }
+            .padding(.horizontal, 8)
+            .padding(.bottom, 4)
         }
     }
 }
@@ -196,73 +192,70 @@ extension WorkSpaceChannelChattingView {
 extension WorkSpaceChannelChattingView {
     
     private func workSpaceChatBottomItemView() -> some View {
-        WithPerceptionTracking {
-            withAnimation {
-                HStack {
-                    ForEach(Array(store.currentDatas.enumerated()), id: \.element.fileName) { index, item in
-                        ZStack(alignment: .topTrailing) {
-                            workSpaceChatFileCaseView(file: item)
-                                .frame(width: 45, height: 45)
-                                
-                            WSXImage.xImage
-                                .resizable()
-                                .renderingMode(.template)
-                                .aspectRatio(1, contentMode: .fit)
-                                .frame(width: 10, height: 10)
-                                .padding(.all, 4)
-                                .background {
-                                    WSXColor.white
-                                }
-                                .clipShape(
-                                    Circle()
-                                )
-                                .overlay(
-                                    Circle()
-                                        .stroke(WSXColor.black.opacity(0.8), lineWidth: 1)
-                                )
-                                .padding(.top, -4)
-                                .padding(.trailing, -4)
-                                .asButton {
-                                    store.send(.dataRemoveToIndex(index))
-                                }
-                        }
-                        
+        withAnimation {
+            HStack {
+                ForEach(Array(store.currentDatas.enumerated()), id: \.element.fileName) { index, item in
+                    ZStack(alignment: .topTrailing) {
+                        workSpaceChatFileCaseView(file: item)
+                            .frame(width: 45, height: 45)
+                            
+                        WSXImage.xImage
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(width: 10, height: 10)
+                            .padding(.all, 4)
+                            .background {
+                                WSXColor.white
+                            }
+                            .clipShape(
+                                Circle()
+                            )
+                            .overlay(
+                                Circle()
+                                    .stroke(WSXColor.black.opacity(0.8), lineWidth: 1)
+                            )
+                            .padding(.top, -4)
+                            .padding(.trailing, -4)
+                            .asButton {
+                                store.send(.dataRemoveToIndex(index))
+                            }
                     }
+                    
                 }
-                .frame(height: 50)
             }
-            .transition(.scale)
-            .animation(.easeInOut, value: store.showChatBottom)
+            .frame(height: 50)
         }
+        .transition(.scale)
+        .animation(.easeInOut, value: store.showChatBottom)
     }
+    @ViewBuilder
     private func workSpaceChatFileCaseView(file: ChatMultipart.File) -> some View {
-        WithPerceptionTracking {
-            switch file.fileType {
-            case .image:
-                Image(uiImage: UIImage(data: file.data) ?? UIImage(systemName: "questionmark")!)
+        switch file.fileType {
+        case .image:
+            Image(uiImage: UIImage(data: file.data) ?? UIImage(systemName: "questionmark")!)
+                .resizable()
+        case .pdf:
+            VStack {
+                Image(systemName: "doc.richtext")
                     .resizable()
-            case .pdf:
-                VStack {
-                    Image(systemName: "doc.richtext")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(WSXColor.white)
-                    Text(file.fileName)
-                }
-                .font(WSXFont.caption)
-            case .zip:
-                VStack {
-                    Image(systemName: "doc.zipper")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .background(WSXColor.white)
-                    Text(file.fileName)
-                }
-                .font(WSXFont.caption)
-            default:
-                Image(systemName: "questionmark")
-                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .background(WSXColor.white)
+                Text(file.fileName)
             }
+            .font(WSXFont.caption)
+        case .zip:
+            VStack {
+                Image(systemName: "doc.zipper")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .background(WSXColor.white)
+                Text(file.fileName)
+            }
+            .font(WSXFont.caption)
+        default:
+            Image(systemName: "questionmark")
+                .resizable()
         }
     }
     
@@ -273,45 +266,43 @@ extension WorkSpaceChannelChattingView {
 extension WorkSpaceChannelChattingView {
     
     private func workSpaceToolView() -> some View {
-        WithPerceptionTracking {
-            withAnimation {
-                HStack {
-                    HStack(spacing: 10) {
-                        // 이미지 먼저 후 -> PDF, 등의 파일 처리
-                        if keyboardTool {
-                            VStack(alignment: .center) {
-                                WSXImage.gallary
-                                    .sideImage()
-                                Text("이미지")
-                                    .font(WSXFont.regu1)
-                            }
-                            .padding(.leading, 10)
-                            .padding(.vertical, 5)
-                            .asButton {
-                                keyboardTool.toggle()
-                                store.send(.showImagePicker)
-                            }
-                            VStack(alignment: .center) {
-                                WSXImage.folder
-                                    .sideImage()
-                                Text("파일")
-                                    .font(WSXFont.regu1)
-                            }
-                            .padding(.leading, 10)
-                            .padding(.vertical, 5)
-                            .asButton {
-                                keyboardTool.toggle()
-                                store.send(.showFilePicker)
-                            }
-                            
+        withAnimation {
+            HStack {
+                HStack(spacing: 10) {
+                    // 이미지 먼저 후 -> PDF, 등의 파일 처리
+                    if keyboardTool {
+                        VStack(alignment: .center) {
+                            WSXImage.gallary
+                                .sideImage()
+                            Text("이미지")
+                                .font(WSXFont.regu1)
                         }
-                        Spacer()
+                        .padding(.leading, 10)
+                        .padding(.vertical, 5)
+                        .asButton {
+                            keyboardTool.toggle()
+                            store.send(.showImagePicker)
+                        }
+                        VStack(alignment: .center) {
+                            WSXImage.folder
+                                .sideImage()
+                            Text("파일")
+                                .font(WSXFont.regu1)
+                        }
+                        .padding(.leading, 10)
+                        .padding(.vertical, 5)
+                        .asButton {
+                            keyboardTool.toggle()
+                            store.send(.showFilePicker)
+                        }
+                        
                     }
-                    .background(WSXColor.white)
+                    Spacer()
                 }
-                .transition(.scale)
-                .animation(.easeInOut, value: keyboardTool)
+                .background(WSXColor.white)
             }
+            .transition(.scale)
+            .animation(.easeInOut, value: keyboardTool)
         }
         
     }
