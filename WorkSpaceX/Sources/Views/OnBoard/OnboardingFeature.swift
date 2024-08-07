38bbb51a95d1d5adfb3280f37c6890fc429149b3
Kim JaeHyung <you3192001@icloud.com>
2024-08-07 17:22:16 +0900

    Refactoring: OnboardingFeature 코드 개선및 가독성

--- WorkSpaceX/Sources/Views/OnBoard/OnboardingFeature.swift
+++ WorkSpaceX/Sources/Views/OnBoard/OnboardingFeature.swift
@@ -26,78 +26,66 @@ struct OnboardingFeature {
     
     @Dependency(\.userDomainRepository) var repository
     @Dependency(\.realmRepository) var realmeRepo
-//    let realmeRepo = RealmRepository()
+
     var body: some ReducerOf<Self> {
-        
         BindingReducer()
         
+        core()
+        .ifLet(\.$onboard, action:\.onboardingLoginFeature) {
+            OnboardingLoginFeature()
+        }
+    }
+}
+
+extension OnboardingFeature {
+    
+    private func core() -> some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .startButtonTapped:
                 state.onboard = OnboardingLoginFeature.State()
-                // 뭐가 있을거임.
-//                return .run { send in
-//                    await send(.testSuccess)
-//                }
-                return .none
+
             case .onboardingLoginFeature(.presented(.delegate(.appleLoginFinish(let model)))): // 애플 로그인시
                 
-                return .run { send in
-                    try await realmeRepo.upsertUserModel(response: model)
-                    await send(.testSuccess)
-                } catch: { error , send in
-                   print(error)
-                }
+                return socialLoginSuccessSideEffect(model: model)
             case .onboardingLoginFeature(.presented(.delegate(.kakaoLoginFinish(let model)))): // 카카오 로그인 시
-                return .run { send in
-                    try await realmeRepo.upsertUserModel(response: model)
-                    await send(.testSuccess)
-                } catch: { error , send in
-                   print(error)
-                }
                 
-            case .onboardingLoginFeature(.presented(.signUpFeature(.presented(.onlyUseParentsUserEntity(let user))))):
-                print("signUpFeatureEvents: \(user)")
+                return socialLoginSuccessSideEffect(model: model)
+            case .onboardingLoginFeature(.presented(.signUpFeature(.presented(.onlyUseParentsUserEntity(let model))))):
+                print("signUpFeatureEvents: \(model)")
                 
-                return .run { send in
-                    try await realmeRepo.upsertUserModel(response: user)
-                    
-                    try await Task.sleep(for:.seconds(1))
-                    
-                    await send(.testSuccess)
-                } catch: { error , send in
-                   print(error)
-                }
-                
+                return socialLoginSuccessSideEffect(model: model)
             case .onboardingLoginFeature(.presented(.emailLoginFeature(.presented(.loginSuccess(let model))))): // 이메일 로그인시
                 print("이메일 로그인 성공 상위뷰 전달 받음")
-                
-                return .run { send in
-                    try await realmeRepo.upsertUserModel(response: model)
-                    try await Task.sleep(for:.seconds(1))
-                    await send(.testSuccess)
-                } catch: { error , send in
-                   print(error)
-                }
+                return socialLoginSuccessSideEffect(model: model)
                 
             case .testSuccess:
                 if ifLogin() {
-                    return .run { send in
-                        await send(.checkedLogin)}
+                    
+                    return .send(.checkedLogin)
+                    
                 } else {
                     state.loginFalid = "로그인중 문제가 발생했습니다 재시도 바랍니다."
                 }
-                return .none
-                
-            case .checkedLogin:
-                return .none
-                
+    
             default :
-                return .none
+                break
             }
+            
+            return .none
         }
-        .ifLet(\.$onboard, action:\.onboardingLoginFeature) {
-            OnboardingLoginFeature()
+    }
+    
+}
+
+extension OnboardingFeature {
+    
+    private func socialLoginSuccessSideEffect(model: UserEntity) -> Effect<Action> {
+        return .run { send in
+            try await realmeRepo.upsertUserModel(response: model)
+            await send(.testSuccess)
+        } catch: { error , send in
+           print(error)
         }
     }
 }
@@ -106,9 +94,6 @@ struct OnboardingFeature {
 extension OnboardingFeature {
     
     private func ifLogin() -> Bool {
-//        if UserDefaultsManager.refreshToken == nil {
-//            return false
-//        }
         if UserDefaultsManager.accessToken == nil {
             return false
         }
